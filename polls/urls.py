eb16506e09c9e401185d4b4a229ca24f90a47f32
shubbuu <sksinha1289@gmail.com>
2024-12-17 16:30:33 +0530

    Chapter-4 - Django doc

--- polls/urls.py
+++ polls/urls.py
@@ -4,15 +4,27 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # eg: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"), # we havn't created template for vote
 
-    # eg: /polls/5/ 
-    path("<int:question_id>/", views.detail, name="detail"),
 
-    # eg: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
 
-    # eg: 
-    path("<int:question_id>/vote/", views.vote, name="vote")
+
+
+
+
+
+    # # eg: /polls/
+    # path("", views.index, name="index"),
+
+    # # eg: /polls/5/ 
+    # path("<int:question_id>/", views.detail, name="detail"),
+
+    # # eg: /polls/5/results/
+    # path("<int:question_id>/results/", views.results, name="results"),
+
+    # # eg: 
+    # path("<int:question_id>/vote/", views.vote, name="vote")
 ]
